cb483d3c0a4713698aaf8bea14dd7fc878a9650b
predictioncourt <predictioncourt@gmail.com>
2026-01-23 00:18:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // ================== CONFIG ==================
 const INITIAL_DELAY = 1500; // 1.5 saniye başlangıç bekleme
-const CIRCLE_LIFETIME = 1600; // 1.2 saniye daire görünme süresi
+const CIRCLE_LIFETIME = 8600; // 1.2 saniye daire görünme süresi
 const MAX_MISSED = 10; // Kaçırılabilecek maksimum daire
 const CIRCLE_RADIUS = 25;
 const CIRCLE_SPEED = 0.7; // Daire hareket hızı
@@ -283,6 +283,7 @@ update();
 document.getElementById("restartBtn").addEventListener("click", () => {
   location.reload();
 });
+
 
 
 
